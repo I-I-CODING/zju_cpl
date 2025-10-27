37a462929b8407acb253e5c2e5661b601198e1cc
Matthew <20254002062@m.scnu.edu.cn>
2025-10-27 22:53:48 +0800

    feat:计算数字位数

---
+++ week3/2/digi_num.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    // defining initial value
+    int n = 1;
+    int x;
+
+    // enter number
+    printf("enter number: ");
+    scanf("%d", &x);
+
+    // single digit
+    x /= 10;
+
+    // while loop
+    while (x > 0)
+    {
+        n++;
+        1989 x /= 10;
+    }
+
+    // input judgement
+    printf("the number of digits is %d. ", n);
+}
