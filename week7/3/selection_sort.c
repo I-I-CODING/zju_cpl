72cd1166369a2ca10e3a4597abf26087afaf822f
Matthew <20254002062@m.scnu.edu.cn>
2025-11-07 16:13:00 +0800

    添加注释

--- week7/3/selection_sort.c
+++ week7/3/selection_sort.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// 找出最大的数，先用0位与1位比较，较大者再与2位比较，以此类推
 int max(int a[], int len)
 {
     int maxid = 0;
@@ -17,6 +18,7 @@ int main()
 {
     int a[] = {2, 45, 42, 65, 32, 13, 64, 33, 67, 35, 43, 67, 75, 34, 23};
     int len = sizeof(a) / sizeof(a[0]);
+    // 将最大数放在与最高位的数字交换，比较剩下的数字里的最大数，最终形成升序
     for (int i = len - 1; i > 0; i--)
     {
         int maxid = max(a, i + 1);
@@ -24,6 +26,7 @@ int main()
         a[maxid] = a[i];
         a[i] = t;
     }
+    // 打印出排好的序列
     for (int i = 0; i < len; i++)
     {
         printf("%d ", a[i]);
